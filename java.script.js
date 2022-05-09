58eff31a32b7f462151eaba211133a467155fd5e
ShevVeronica <103795178+ShevVeronica@users.noreply.github.com>
2022-05-09 19:04:46 -0300

    Add files via upload

---
+++ java.script.js
@@ -0,0 +1,36 @@
+
+function Encriptar() {
+    var texto = document.getElementById("inputtexto").value.toLowerCase();
+    var txtcifrado = texto.replace(/e/igm,"enter");
+    var txtcifrado = txtcifrado.replace(/o/igm,"ober");
+    var txtcifrado = txtcifrado.replace(/i/igm,"imes");
+    var txtcifrado = txtcifrado.replace(/a/igm,"ai");
+    var txtcifrado = txtcifrado.replace(/u/igm,"ufat");
+    document.getElementById("imgDerecha").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("texto2").innerHTML = txtcifrado;
+    document.getElementById("copiar").style.display = "inherit";
+  }
+  
+  function Desencriptar() {
+    var texto = document.getElementById("inputtexto").value.toLowerCase();
+    var txtcifrado = texto.replace(/enter/igm,"e");
+    var txtcifrado = txtcifrado.replace(/ober/igm,"o");
+    var txtcifrado = txtcifrado.replace(/imes/igm,"i");
+    var txtcifrado = txtcifrado.replace(/ai/igm,"a");
+    var txtcifrado = txtcifrado.replace(/ufat/igm,"u");
+    document.getElementById("imgDerecha").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("texto2").innerHTML = txtcifrado;
+    document.getElementById("copiar").style.display = "inherit";
+  }
+  
+  function copia() {
+    var contenido = document.querySelector("#texto2");
+    contenido.select();
+    document.execCommand('copy');
+    alert("Se a Copiado!");
+  }
+  
